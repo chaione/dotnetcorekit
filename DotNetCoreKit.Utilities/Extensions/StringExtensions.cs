4527f2b5c3e49e249abd1b56b4d666a564b0e405
DESKTOP-SHRUBCD\Jekee Desai <jekee.desai@gmail.com>
2017-11-01 11:44:57 -0500

    adds common utilities project for referencing across multiple projects internally so that we can have a central location to serve similar independent functions from

---
+++ DotNetCoreKit.Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StringExtensions.cs" company="ChaiOne">
+// Copyright (c) ChaiOne. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DotNetCoreKit.Utilities.Extensions
+{
+    /// <summary>
+    /// Contains a list of extensions that can be used for common purposes.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Determines whether a specified string is not null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="source">The source string that we should check validity of.</param>
+        /// <returns>Returns true if the string is valid and false if is invalid</returns>
+        public static bool HasValue(this string source) => !string.IsNullOrWhiteSpace(source) && source?.Trim().Length != 0;
+    }
+}
